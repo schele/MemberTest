30aa7bd18ba2677588e0e1fac1745737e6704176
Carl Schéle <carl.schele@asurgent.se>
2025-10-15 17:19:28 +0200

    fixed a lock bug

---
+++ Business/Extensions/MemberExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json;
+using Umbraco.Cms.Core.Models;
+
+namespace MembersTestUmbraco16.Business.Extensions
+{
+    public static class MemberExtensions
+    {
+        public static string? ImageUrl(this IMember member, string alias, Umbraco.Cms.Web.Common.UmbracoHelper umbraco)
+        {
+            if (member == null || umbraco == null) return null;
+
+            var json = member.GetValue<string>(alias);
+
+            if (string.IsNullOrWhiteSpace(json)) return null;
+
+            try
+            {
+                var doc = JsonDocument.Parse(json);
+
+                if (doc.RootElement.GetArrayLength() == 0) return null;
+
+                var mediaKey = doc.RootElement[0].GetProperty("mediaKey").GetGuid();
+                var mediaItem = umbraco.Media(mediaKey);
+
+                return mediaItem?.Url();
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
